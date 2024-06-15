f8dba5ebd6a049a6b7c40b8d512ca8ce5082bcc0
migueld2000 <migueld2000@gmail.com>
2024-06-15 18:33:57 -0500

    textos y correcion de seciones

--- public_html/js/scripts.js
+++ public_html/js/scripts.js
@@ -15,10 +15,43 @@ function closeOtherCollapses(openId) {
         }
     });
 }
+//botn blog
+
+function toggleButtonStyles(clickedButton, clickedButtonId) {
+    const buttons = document.querySelectorAll('.nav-link');
+    buttons.forEach(button => {
+        if (button.id === clickedButtonId) {
+            button.classList.add('primary-button');
+            button.classList.remove('secondary-button');
+        } else {
+            button.classList.remove('primary-button');
+            button.classList.add('secondary-button');
+        }
+    });
+}
+
+//contenido dle blog
+function toggleButtonStyles(clickedButton, targetPaneId) {
+    const buttons = document.querySelectorAll('.nav-link');
+    buttons.forEach(button => {
+        button.classList.remove('primary-button');
+        button.classList.add('secondary-button');
+    });
+    clickedButton.classList.add('primary-button');
+    clickedButton.classList.remove('secondary-button');
+
+    const panes = document.querySelectorAll('.tab-pane');
+    panes.forEach(pane => {
+        pane.classList.remove('show', 'active');
+    });
+    document.getElementById(targetPaneId).classList.add('show', 'active');
+}
+
+
 
 //boton de scroll
 function closeOtherCollapses(currentId) {
-    const collapses = ['collapseDesayuno', 'collapseAlmuerzos', 'collapseComidas'];
+    const collapses = ['collapseDesayuno', 'collapseAlmuerzos'];
     collapses.forEach(id => {
         if (id !== currentId) {
             const element = document.getElementById(id);
@@ -152,80 +185,3 @@ function scrollToTop() {
     document.documentElement.scrollTop = 0; // Para Chrome, Firefox, IE y Opera
 }
 
-//animacion de promociones
-// script.js
-document.addEventListener('DOMContentLoaded', function() {
-    const elements = document.querySelectorAll('.slide-in-on-scroll');
-
-    function checkVisibility() {
-        const triggerPoint = window.innerHeight / 1.1; // Ajusta este valor según sea necesario
-
-        elements.forEach(element => {
-            const elementTop = element.getBoundingClientRect().top;
-
-            if (elementTop < triggerPoint) {
-                element.classList.add('visible');
-            }
-        });
-    }
-
-    window.addEventListener('scroll', checkVisibility);
-    checkVisibility(); // Verifica la visibilidad al cargar la página
-});
-
-//animacion de eventos
-document.addEventListener('DOMContentLoaded', function() {
-    const elements = document.querySelectorAll('.scale-in-on-scroll');
-
-    function checkVisibility() {
-        const triggerPoint = window.innerHeight / 1.1; // Ajusta este valor según sea necesario
-
-        elements.forEach(element => {
-            const elementTop = element.getBoundingClientRect().top;
-
-            if (elementTop < triggerPoint) {
-                element.classList.add('visible');
-            }
-        });
-    }
-
-    window.addEventListener('scroll', checkVisibility);
-    checkVisibility(); // Verifica la visibilidad al cargar la página
-});
-
-
-//animacion de menu
-document.addEventListener('DOMContentLoaded', function() {
-    const elements = document.querySelectorAll('.slide-left-on-scroll');
-
-    function checkVisibility() {
-        const triggerPoint = window.innerHeight / 1.1; // Ajusta este valor según sea necesario
-
-        elements.forEach(element => {
-            const elementTop = element.getBoundingClientRect().top;
-
-            if (elementTop < triggerPoint) {
-                element.classList.add('visible');
-            }
-        });
-    }
-
-    function adjustPosition() {
-        elements.forEach(element => {
-            if (element.classList.contains('visible')) {
-                const buttons = element.querySelectorAll('button');
-                const secondButton = buttons[1];
-                const screenWidth = window.innerWidth;
-                const secondButtonWidth = secondButton.offsetWidth;
-
-                // Calcular la posición para centrar el segundo botón
-                const translateX = (screenWidth - secondButtonWidth) / 2 - secondButton.getBoundingClientRect().left;
-                element.style.transform = `translateX(${translateX}px)`;
-            }
-        });
-    }
-
-    window.addEventListener('scroll', checkVisibility);
-    window.addEventListener('resize', adjustPosition);
-    checkVisibility(); // Verifica la visibilidad al cargar la página
-});
